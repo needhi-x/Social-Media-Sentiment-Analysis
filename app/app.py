fe0cdf8a1fe64028ffde389ac73dd5872fdd9116
Nidhi Apotikar <275444414+needhi-x@users.noreply.github.com>
2026-04-30 20:36:23 +0530

    fix nltk deployment issue

--- app/app.py
+++ app/app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import nltk
+
+try:
+    nltk.data.find('sentiment/vader_lexicon')
+except LookupError:
+    nltk.download('vader_lexicon')
 from nltk.sentiment import SentimentIntensityAnalyzer
 
 # Initialize VADER
